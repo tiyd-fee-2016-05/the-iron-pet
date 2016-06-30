6a4fbba0abfef1251c468a76e92d223f3a404546
SINOVIA <somayfield@gmail.com>
2016-06-30 09:03:26 -0400

    Sinovia addition

--- js/app.js
+++ js/app.js
@@ -5,38 +5,117 @@ mainApp.config(function($routeProvider) {
   //Consumer View Routes
     .when('/home', {
       templateUrl: 'home.html',
-      // controller: 'AppointmentController'
+      // controller: 'PetController'
     })
     .when('/availablePets', {
       templateUrl: 'consumerviews/allPets.html',
-      // controller: 'AppointmentController'
+      controller: 'PetController'
     })
     .when('/MyCouch', {
       templateUrl: 'consumerviews/couchView.html',
-      // controller: 'AppointmentController'
+      // controller: 'PetController'
     })
     .when('/petdetails', {
       templateUrl: 'consumerviews/petDetails.html',
-      // controller: 'AppointmentController'
+          // controller: 'PetController'
     })
     .when('/checkout', {
       templateUrl: 'consumerviews/petTime.html',
-      // controller: 'AppointmentController'
+          // controller: 'PetController'
     })
     //Admin View Routes
     .when('/admin', {
       templateUrl: 'adminviews/addPetAdmin.html',
-      // controller: 'AppointmentController'
+          // controller: 'PetController'
     })
     .when('/admindetails', {
       templateUrl: 'adminviews/petAdminDetails.html',
-      // controller: 'AppointmentController'
+          // controller: 'PetController'
     })
     .when('/adminAddPet', {
       templateUrl: 'adminviews/petListAdmin.html',
-      // controller: 'AppointmentController'
+          // controller: 'PetController'
     })
     .otherwise({
       redirectTo: '/home'
     });
+});
+
+
+mainApp.controller('PetController', function($scope){
+  $scope.petList =
+[
+  {PetName: 'Friendly',
+  PetBreed:'Schnauzer',
+  FavActivity:'Napping',
+  FavSnack:'Beggin Strips',
+  PetDescription:'A loveable young lady with a heart of gold!'},
+  {PetName: 'Leo',
+  PetBreed:'Schnauzer2',
+  FavActivity:'Napping2',
+  FavSnack:'Beggin Strips2',
+  PetDescription:'A loveable young lady with a heart of gold!2'},
+
+  {PetName: 'Milo',
+  PetBreed:'Schnauzer2',
+  FavActivity:'Napping2',
+  FavSnack:'Beggin Strips2',
+  PetDescription:'A loveable young lady with a heart of gold!2'},
+
+  {PetName: 'Barry',
+  PetBreed:'Schnauzer2',
+  FavActivity:'Napping2',
+  FavSnack:'Beggin Strips2',
+  PetDescription:'A loveable young lady with a heart of gold!2'},
+
+  {PetName: 'Buster',
+  PetBreed:'Schnauzer',
+  FavActivity:'Napping',
+  FavSnack:'Beggin Strips',
+  PetDescription:'A loveable young lady with a heart of gold!'},
+
+  {PetName: 'Hank',
+  PetBreed:'Schnauzer2',
+  FavActivity:'Napping2',
+  FavSnack:'Beggin Strips2',
+  PetDescription:'A loveable young lady with a heart of gold!2'},
+
+  {PetName: 'Reggie',
+  PetBreed:'Schnauzer2',
+  FavActivity:'Napping2',
+  FavSnack:'Beggin Strips2',
+  PetDescription:'A loveable young lady with a heart of gold!2'},
+
+  {PetName: 'Nathaniel',
+  PetBreed:'Schnauzer2',
+  FavActivity:'Napping2',
+  FavSnack:'Beggin Strips2',
+  PetDescription:'A loveable young lady with a heart of gold!2'},
+
+  {PetName: 'Captain Morgan',
+  PetBreed:'Schnauzer',
+  FavActivity:'Napping',
+  FavSnack:'Beggin Strips',
+  PetDescription:'A loveable young lady with a heart of gold!'},
+
+  {PetName: 'Tyrion',
+  PetBreed:'Schnauzer2',
+  FavActivity:'Napping2',
+  FavSnack:'Beggin Strips2',
+  PetDescription:'A loveable young lady with a heart of gold!2'},
+
+  {PetName: 'Hermione',
+  PetBreed:'Schnauzer2',
+  FavActivity:'Napping2',
+  FavSnack:'Beggin Strips2',
+  PetDescription:'A loveable young lady with a heart of gold!2'},
+
+  {PetName: 'Katniss',
+  PetBreed:'Schnauzer2',
+  FavActivity:'Napping2',
+  FavSnack:'Beggin Strips2',
+  PetDescription:'A loveable young lady with a heart of gold!2'},
+]
+  $scope.message = "This is a test message."
+
 });

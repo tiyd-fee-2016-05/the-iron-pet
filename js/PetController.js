062c20612e0f58102cd31c5df8fff493f4ec490c
SINOVIA <somayfield@gmail.com>
2016-07-03 11:16:04 -0400

    Function for new pet push

--- js/PetController.js
+++
@@ -1,9 +0,0 @@
-$http({
-    method: 'GET',
-    url: 'http://localhost:3007/dogs'
-}).then(function successCallback(data) {
-    console.log(data)
-}, function errorCallback(response) {
-    // called asynchronously if an error occurs
-    // or server returns response with an error status.
-});
--- js/admin.js
+++ js/admin.js
@@ -1,33 +1,22 @@
-mainApp.controller('PetController', function($scope, $http){
-​
- $('.NewPuppy').on('submit', function (e) {
-​
-    $http.post("http://localhost:3007/dogs", {
-​
-      var newPetName = $('input[name="savePetName"]').val(),
-      var newPetPhoto = $('input[name="savePetPhoto"]').val(),
-      var newPetBreed= $('input[name="savePetBreed"]').val(),
-      var newPetPrice= $('input[name="savePetPrice"]').val(),
-      var newPetSnack= $('input[name="savePetActivity"]').val(),
-      var newPetActivity= $('input[name="savePetSnack"]').val(),
-      var newPetDescription = $('input[name="savePetDesc"]').val(),
-​
-      ["PetNumber":"12",
-      "PetName" : newPetName,
-      "PetPhoto": newPetPhoto,
-      "PetBreed": newPetBreed,
-      "PetPrice": newPetPrice,
-      "FavActivity": newPetActivity,
-      "FavSnack": newPetSnack,
-      "PetDescription": newPetDescription
-    ]
+$('.NewPuppy').click(function (e) {
+  e.PreventDefault();
+
+  $.post("http://localhost:3007/dogs",
+
+  {
+      "PetNumber":"12",
+      "PetName" : $('input[name="savePetName"]').val(),
+      "PetPhoto": $('input[name="savePetPhoto"]').val(),
+      "PetBreed": $('input[name="savePetBreed"]').val(),
+      "PetPrice": $('input[name="savePetPrice"]').val(),
+      "FavActivity": $('input[name="savePetActivity"]').val(),
+      "FavSnack": $('input[name="savePetSnack"]').val(),
+      "PetDescription": $('input[name="savePetDesc"]').val(),
+
     }
     .success(function(data, status, headers, config) {
-        $scope.data = data;
-        console.log("Successful post!")
-    }).error(function(data, status, headers, config) {
-        $scope.status = status;
+
+        console.log("Successful post!" + data)
+      });)
     });
-​
-    })
-Add Comment
+});
